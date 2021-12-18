cf2f9e225e6f8cfd9fb6a9944a4e937ae09ab575
blend4unity <34580656+blend4unity@users.noreply.github.com>
2021-12-18 12:43:12 +0530

    lastcommit

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -4,6 +4,27 @@ import {Feature } from '../../components';
 
 const featuresData = [
     {
+<<<<<<< HEAD
+      title: 'REAL TIME REPORT',
+      text: 'We provide our real time reporting panel access to every publisher. Publishers can view their traffic reports 24/7 by using our panel.',
+    },
+    {
+      title: 'HIGHEST PAYOUTS',
+      text: 'We close the campaign deals with an advertiser on a high payout because of the quality traffic that our Affiliate members generate.',
+    },
+    {
+      title: 'EXCLUSIVE OFFERS',
+      text: 'We have most of the exclusive offers from the world. Check our offer list and apply the one that suits your traffic the best. Check out our latest offers.',
+    },
+   
+    {
+      title: 'TIMELY PAYMENT',
+      text: 'The payment of publishers is usually made in 30 days (NET30) after the end of the month and after receiving a payment from the Advertiser.',
+    },
+    {
+      title: 'CREATIVE SUPPORT',
+      text: 'LTPLDIGITAL has a team of creative support experts that design appealing landing pages and content that can be used to boost their campaigns.',
+=======
       title: 'Improving end distrusts instantly',
       text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
     },
@@ -18,6 +39,7 @@ const featuresData = [
     {
       title: 'Really boy law county',
       text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush..',
+>>>>>>> 9cc6d4455690b7e567425e2116a5e0072ffbc7f5
     },
   ];
 
--- src/containers/whatGTP3/WhatGPT3.jsx
+++ src/containers/whatGTP3/WhatGPT3.jsx
@@ -6,11 +6,18 @@ const WhatGPT3 = () => {
     return (
         <div className="gpt3__whatgpt3 section__margin" id="products">
            <div className="gpt3__whatgpt3-feature">
+<<<<<<< HEAD
+                <Feature title="LinkedWide " text="LTPLDIGITAL offers advertisers a variety of services to meet the unique requirements of their business. Just briefly state requirements and we will get back to you with the best offer for your business to succeed.
+
+Pre Launch and Post Launch plans have been developed to make a thorough analysis of the market and maximize the LTV.
+LTPLDIGITAL will save your time and money and provide the desired results." />
+=======
                 <Feature title="LinkedWide " text="We so opinion friends me message as delight.
                  Whole front do of plate heard oh ought. 
                  His defective nor convinced residence own.
                   Connection has put impossible own apartments boisterous. 
                   At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+>>>>>>> 9cc6d4455690b7e567425e2116a5e0072ffbc7f5
 
             </div>
             <div className="gpt3__whatgpt3-heading">
@@ -18,10 +25,33 @@ const WhatGPT3 = () => {
                 <p><a href='http://affiliate.ltpldigital.com/m/access'>Join as Affliate</a></p>
             </div>
             <div className="gpt3__whatgpt3-container">
+<<<<<<< HEAD
+                <Feature title="Digital Marketing" text="SOCIAL NETWORKING MARKETING
+                                                         GOOGLE ADSENSE TRAINING
+                                                         CPA/CPL/CPC MARKETING
+                                                         AFFILIATE MARKETING
+                                                         E-COMMERCE" />
+                <Feature title="Insurance" text="GENERAL INSURANCE
+                                                 STUDENT INSURANCE
+                                                 FARMER INSURANCE
+                                                 HEALTH INSURANCE
+                                                 CORP INSURANCE
+                                                 LIFE INSURANCE" />
+                <Feature title="CA Services" text="COMPANY REGISTRATION & ALL ROC WORK
+                                                   GST REGISTRATION & FILING
+                                                   MSME REGISTRATION
+                                                   INCOME TAX FILIING
+                                                   FSSI REGISTRATION" />
+                <Feature title="Field Survey" text="KYC & EKYC WORK FOR DIGITAL PAYMENT BANK
+                                                    DATA COLLECTION WORK
+                                                    POI & GIS SURVEY WORK
+                                                    METER READING WORK " />
+=======
                 <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. " />
                 <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments" />
                 <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
                 <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. " />
+>>>>>>> 9cc6d4455690b7e567425e2116a5e0072ffbc7f5
             </div>
         </div>
     )
